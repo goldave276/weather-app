bc66a93047b3c1a097cf197632fb636287e6e38c
goldave276 <daveably01@gmail.com>
2026-07-26 23:12:06 +0000

    third changes

--- src/components/weatherCard.jsx
+++ src/components/weatherCard.jsx
@@ -2,7 +2,7 @@ function WeatherCard({weather, city}) {
     const formattedTime = new Date(weather.current_weather.time).toLocaleString();
     const {temperature, windspeed, winddirection, time, code} = weather.current_weather;
     return (
-        <div>
+        <div className="weather-card">
             <h2>Données météo pour {city}</h2>
             <p>Température : {temperature}°C</p>
             <p>Vitesse du vent : {windspeed} km/h</p>
